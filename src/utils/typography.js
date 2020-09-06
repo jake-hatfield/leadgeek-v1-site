cad9848be2b3a90872314b1a63f94a04bbda4f3a
Jacob Hatfield <jacobmhatfield@gmail.com>
2020-09-06 13:51:55 -0500

    chore: font fix?

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,7 +3,7 @@ import Typography from "typography"
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.666,
-  googleFonts: [{ name: "Inter", styles: [] }],
+  googleFonts: [{ name: "Inter", styles: ["300, 400, 500, 600, 700, 900"] }],
   headerFontFamily: ["Inter", "sans-serif"],
   bodyFontFamily: ["Inter", "sans-serif"],
 })
