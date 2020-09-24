49c07f75de7d4c05b1aa17d3b9fad4d5ec81bb2d
Jacob Hatfield <jacobmhatfield@gmail.com>
2020-09-24 15:01:48 -0500

    add: favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/leadgeek-logo.png`, // This path is relative to the root of the site.
       },
     },
     {
