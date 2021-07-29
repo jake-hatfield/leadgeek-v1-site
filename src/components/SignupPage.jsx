11157cc0f3d9a8454e5cca1977bb5fe5c7fa2033
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-07-29 13:12:37 -0500

    sign up page updates

--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -32,8 +32,8 @@ const SignupPage = ({
     <section className="relative text-gray-700">
       <div className="absolute inset-x-0 top-0 border-b-8 border-purple-500 z-10"></div>
       <div className="relative min-h-screen py-6 h-100 md:min-h-screen bg-splatter">
-        <article className="mx-auto relative container">
-          <header className="relative z-20">
+        <article className="mx-auto container">
+          <header>
             <h1 className="pt-4 lg:pt-6 font-bold text-xl lg:text-2xl group">
               <Link
                 to={`/pricing/`}
@@ -44,44 +44,25 @@ const SignupPage = ({
               </Link>
             </h1>
           </header>
-        </article>
-        <div className="relative md:min-h-screen container">
-          <div
-            className={`hidden lg:block absolute top-0 left-0 py-1 px-2 handwritten text-lg xl:text-xl rounded-lg bg-gray-900 text-teal-300 transform translate-x-12 translate-y-40 shadow-tealMd text-center transition-main w-56 -rotate-6 hover:-rotate-3`}
-          >
-            <blockquote>
-              "Would definitely recommend if you want to take the guess work out
-              of sourcing for FBA" - <cite>William</cite>
-            </blockquote>
-          </div>
-          <div
-            className={`hidden lg:block absolute bottom-0 right-0 py-1 px-2 handwritten text-lg xl:text-xl rounded-lg bg-gray-900 text-teal-300 transform -translate-y-48 -translate-x-6 shadow-tealMd text-center transition-main w-56 rotate-6 hover:rotate-3`}
-          >
-            <blockquote>
-              "It has been the best decision I have made in acquiring your
-              list." - <cite>Frank</cite>
-            </blockquote>
-          </div>
-        </div>
-
-        <div className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-y-1/2 md:-translate-x-1/2 mt-6 md:mt-0 container">
-          <div className="mx-auto h-full w-full max-w-md">
-            <div className="py-4 lg:py-6 px-6 bg-white rounded-lg border border-gray-900 transition-main shadow-dark">
-              <Elements stripe={stripePromise}>
-                <CheckoutForm
-                  plan={plan}
-                  price={price}
-                  productSelected={productSelected}
-                  featureList={featureList}
-                  discount={discount}
-                  formData={formData}
-                  setFormData={setFormData}
-                  onSuccessfulCheckout={() => onSuccessfulCheckout()}
-                />
-              </Elements>
+          <div className="md:absolute md:top-1/2 md:left-1/2 md:transform md:-translate-y-1/2 md:-translate-x-1/2 mt-6 xl:mt-10">
+            <div className="mx-auto h-full w-full max-w-md">
+              <div className="py-4 lg:py-6 px-6 bg-white rounded-lg border border-gray-900 transition-main shadow-dark">
+                <Elements stripe={stripePromise}>
+                  <CheckoutForm
+                    plan={plan}
+                    price={price}
+                    productSelected={productSelected}
+                    featureList={featureList}
+                    discount={discount}
+                    formData={formData}
+                    setFormData={setFormData}
+                    onSuccessfulCheckout={() => onSuccessfulCheckout()}
+                  />
+                </Elements>
+              </div>
             </div>
           </div>
-        </div>
+        </article>
       </div>
     </section>
   )
