173fbcf523d52c4f59a97313ec12fd8f6516c967
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-08-09 17:59:56 -0500

    cookie button

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -36,7 +36,7 @@ const Layout = ({ children, location, popup }) => {
         containerClasses="py-4 mx-4 fixed bottom-0 md:left-1/2 md:flex md:items-center md:justify-center w-auto container z-40 bg-gray-900 rounded-lg mb-4 lg:mb-6 shadow-tealSm transform md:-translate-x-1/2 md:whitespace-nowrap"
         contentClasses="lg:mr-4 text-teal-300 text-sm"
         buttonWrapperClasses="mt-4 md:mt-0 md:ml-4 flex-none"
-        buttonClasses="block w-full md:w-auto cta-link inter"
+        buttonClasses="block w-full md:w-auto py-2 px-3 rounded-lg shadow-md hover:shadow-lg border border-purple-500 hover:border-purple-600 bg-purple-500 font-semibold text-xs md:text-sm hover:bg-purple-600 text-white transition-main ring-purple inter"
       >
         <span role="img" aria-label="Waving Emoji">
           👋
