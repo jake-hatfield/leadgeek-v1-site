0b69f5fda46d5ae0cbbdee5f7063ebb3fc733f68
Jacob Hatfield <jacobmhatfield@gmail.com>
2021-08-18 13:44:48 -0500

    add gtm

--- plugins/gatsby-plugin-anti-flicker/gatsby-ssr.js
+++ plugins/gatsby-plugin-anti-flicker/gatsby-ssr.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+const containerId = "GTM-NNVJ2DH"
+
 const createDataLayer = () => ({
   __html: "window.dataLayer = window.dataLayer || []",
 })
@@ -11,12 +13,22 @@ const optimizeAntiFlickerScript = containerId => ({
 h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
 (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
 })(window,document.documentElement,'async-hide','dataLayer',2000,
-{'GTM-NNVJ2DH':true});`,
+{${containerId}:true});`,
 })
+
+const googleTagManagerScript = containerId => ({
+  __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NNVJ2DH');`,
+})
+
 const customHeadComponents = [
   <script dangerouslySetInnerHTML={createDataLayer()} />,
   <style dangerouslySetInnerHTML={optimizeAntiFlickerStyle()} />,
-  <script dangerouslySetInnerHTML={optimizeAntiFlickerScript("GTM-NNVJ2DH")} />,
+  <script dangerouslySetInnerHTML={optimizeAntiFlickerScript(containerId)} />,
+  <script dangerouslySetInnerHTML={googleTagManagerScript(containerId)} />,
 ]
 export const onPreRenderHTML = ({
   getHeadComponents,
